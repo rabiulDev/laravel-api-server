dcee411ae2812ba700f85f1be680c51bf95e582d
rabiulDev <rabiul.dev@gmail.com>
2024-04-16 18:20:19 +0600

    create user api route and initialize UserController

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/users', function (Request $request) {
+    dump($request);
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'Hehehe',
+    ]);
+});
+
+Route::get('/users/{user}', function (\App\Models\User $user, Request $request) {
+    dump($request);
+    return new \Illuminate\Http\JsonResponse([
+        'data' => $user,
+    ]);
+});
+
+Route::post('/users', function () {
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'posted',
+    ]);
+});
+
+Route::patch('/users/{user}', function (\App\Models\User $user) {
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'patched',
+    ]);
+});
+
+Route::delete('/users/{user}', function (\App\Models\User $user) {
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'deleted',
+    ]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
