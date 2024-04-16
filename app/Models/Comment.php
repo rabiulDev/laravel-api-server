55f5795d58f2b04f67470fe35c8cbf4c320ad880
rabiulDev <rabiul.dev@gmail.com>
2024-04-16 16:33:22 +0600

    define relations fields to model and add accessor and mutator in post model then practice with many to many field attach, detach, toggle, sync, syncWithoutDetach method in tinker

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -11,4 +11,9 @@ class Comment extends Model
     protected $casts = [
         'body' => 'array'
     ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
 }
