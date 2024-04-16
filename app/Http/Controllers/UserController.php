8eeff384e71197ea9eb2b9c15ed7a8cf881baa10
rabiulDev <rabiul.dev@gmail.com>
2024-04-16 18:56:05 +0600

    use controller for user api route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use \Illuminate\Http\Request;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Models\User;
@@ -11,55 +12,65 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        //
+        return new \Illuminate\Http\JsonResponse([
+            'data' => 'Hehehe',
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StorePostRequest  $request
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
-        //
+        return new \Illuminate\Http\JsonResponse([
+            'data' => 'Created',
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show(Post $post)
+    public function show(User $user)
     {
-        //
+        return new \Illuminate\Http\JsonResponse([
+            'data' => $user,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdatePostRequest  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(Request $request, User $user)
     {
-        //
+        return new \Illuminate\Http\JsonResponse([
+            'data' => 'Updated',
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy(Post $post)
+    public function destroy(User $user)
     {
-        //
+        return new \Illuminate\Http\JsonResponse([
+            'data' => 'Deleted',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,37 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/users', function (Request $request) {
-    dump($request);
-    return new \Illuminate\Http\JsonResponse([
-        'data' => 'Hehehe',
-    ]);
-});
+Route::get('/users', [UserController::class, 'index']);
 
-Route::get('/users/{user}', function (\App\Models\User $user, Request $request) {
-    dump($request);
-    return new \Illuminate\Http\JsonResponse([
-        'data' => $user,
-    ]);
-});
+Route::get('/users/{user}', [UserController::class, 'show']);
 
-Route::post('/users', function () {
-    return new \Illuminate\Http\JsonResponse([
-        'data' => 'posted',
-    ]);
-});
+Route::post('/users', [UserController::class, 'store']);
 
-Route::patch('/users/{user}', function (\App\Models\User $user) {
-    return new \Illuminate\Http\JsonResponse([
-        'data' => 'patched',
-    ]);
-});
+Route::patch('/users/{user}', [UserController::class, 'update']);
 
-Route::delete('/users/{user}', function (\App\Models\User $user) {
-    return new \Illuminate\Http\JsonResponse([
-        'data' => 'deleted',
-    ]);
-});
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
