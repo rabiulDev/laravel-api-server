d23744539eb9f0b33975f1cf17781e30c27b74bc
rabiulDev <rabiul.dev@gmail.com>
2024-04-24 19:37:50 +0600

    mail class added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+if(\Illuminate\Support\Facades\App::environment('local')){
+
+    Route::get('/playground', function (){
+        return (new \App\Mail\WelcomeMail())->render();
+    });
+}
